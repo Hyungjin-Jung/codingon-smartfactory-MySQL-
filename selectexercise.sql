0ec8d4e42cc0cb3bd0f17525fc4991a26fede179
Hyungjin-Jung <1hyungjin1@naver.com>
2023-03-08 14:43:32 +0900

    SELECT 예제 수정

--- selectexercise.sql
+++ selectexercise.sql
@@ -2,26 +2,40 @@ use selectexercise;
 
 SELECT * FROM user;
 
--- 모든 회원목록을 가져오는데, 이때 birthday 컬럼의 값을 기준으로 오름차순 정렬하여 가져오시오. 
+-- 1. 모든 회원목록을 가져오는데, 이때 birthday 컬럼의 값을 기준으로 오름차순 정렬하여 가져오시오. 
 SELECT * FROM user ORDER BY birthday;
+SELECT * FROM user ORDER BY birthday ASC;
 
--- 회원 목록 중 gender 컬럼의 값이 "M"인 회원목록을 가져오는데, 이때 name 컬럼의 값을 기준으로 내림차순 정렬하여 가져오시오. 
+-- 2. 회원 목록 중 gender 컬럼의 값이 "M"인 회원목록을 가져오는데, 이때 name 컬럼의 값을 기준으로 내림차순 정렬하여 가져오시오. 
 SELECT * FROM user WHERE gender = 'M' ORDER BY name DESC;
+SELECT * FROM user WHERE gender IN ('M') ORDER BY name DESC;
 
--- 1990년대 태어난 회원의 id, name 컬럼을 가져와 목록으로 보여주시오. 
+-- 3. 1990년대 태어난 회원의 id, name 컬럼을 가져와 목록으로 보여주시오. 
 SELECT id, name FROM user WHERE birthday >= '1990-01-01' AND birthday <= '1999-12-31';
+SELECT id, name FROM user WHERE birthday BETWEEN '1990-01-01' AND '1999-12-31';
+SELECT id, name FROM user WHERE birthday like '199%';
 
--- 6월생 회원의 목록을 birthday 기준으로 오름차순 정렬하여 가져오시오.
-SELECT * FROM user WHERE birthday LIKE '______6' ORDER BY birthday;
+-- 4. 6월생 회원의 목록을 birthday 기준으로 오름차순 정렬하여 가져오시오.
+SELECT * FROM user WHERE birthday LIKE '____-06-__' ORDER BY birthday;
+SELECT * FROM user WHERE birthday LIKE '%-06-%' ORDER BY birthday;
+SELECT * FROM user WHERE birthday LIKE '%-06-%' ORDER BY birthday ASC;
 
--- gender 컬럼의 값이 "M"이고, 1970년대에 태어난 회원의 목록을 가져오시오. 
-SELECT * FROM user WHERE gender LIKE 'M' AND birthday >= '1970-01-01' AND birthday <= '1979-12-31';
+-- 5. gender 컬럼의 값이 "M"이고, 1970년대에 태어난 회원의 목록을 가져오시오. 
+SELECT * FROM user 
+	WHERE gender LIKE 'M' AND 
+		birthday >= '1970-01-01' AND birthday <= '1979-12-31';	-- 가독성을 위해 엔터
+SELECT * FROM user WHERE gender LIKE 'M' AND birthday BETWEEN '1970-01-01' AND '1979-12-31';
+SELECT * FROM user WHERE gender LIKE 'M' AND birthday LIKE '197%';
 
--- 모든 회원목록 중 age를 기준으로 내림차순 정렬하여 가져오는데, 그때 처음 3개의 레코드만 가져오시오.
+-- 6. 모든 회원목록 중 age를 기준으로 내림차순 정렬하여 가져오는데, 그때 처음 3개의 레코드만 가져오시오.
  SELECT * FROM user ORDER BY age DESC LIMIT 3;
+ SELECT * FROM user ORDER BY age DESC LIMIT 0, 3;
+ SELECT * FROM user ORDER BY age DESC LIMIT 3 OFFSET 0;
  
- -- 모든 회원 목록 중 나이가 25 이상 50 이하인 회원의 목록을 출력하시오.
+ -- 7. 모든 회원 목록 중 나이가 25 이상 50 이하인 회원의 목록을 출력하시오.
  SELECT * FROM user WHERE age BETWEEN 25 AND 50; 
+ SELECT * FROM user WHERE age >= 25 AND age <= 50; 
+ SELECT * FROM user WHERE age > 24 AND age < 51; 
 
 
 
