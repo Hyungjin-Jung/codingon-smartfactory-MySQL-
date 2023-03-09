96e41443c198f966c92bf6fdc186c47187c28f5c
Hyungjin-Jung <1hyungjin1@naver.com>
2023-03-09 17:36:29 +0900

    DML 실습

--- index_03.sql
+++ index_03.sql
@@ -96,8 +96,48 @@ ALTER TABLE new_customer DROP n_name;
 DROP TABLE new_customer;
 DROP TABLE new_orders;
 
+-- 실습
+
+CREATE TABLE new_user
+(
+	id VARCHAR(10) PRIMARY KEY, 
+	pw VARCHAR(20) NOT NULL,
+	name CHAR(5) NOT NULL,
+	gender CHAR(1),
+	birthday DATE NOT NULL,
+    age INT NOT NULL
+);
+DESC new_user;
+SELECT * FROM new_user;
+
+DROP TABLE new_user;
+
+CREATE TABLE member
+(
+	id VARCHAR(20) PRIMARY KEY, 
+	name VARCHAR(5) NOT NULL,
+    age INT,
+	gender VARCHAR(2) NOT NULL,
+	email VARCHAR(50),
+    promotion VARCHAR(2) DEFAULT "x"
+);
+DESC member;
+SELECT * FROM member;
 
 
 
+-- member 속성 수정 
 
+-- (1) id 값 형식 변경 
+ALTER TABLE member MODIFY id VARCHAR(10);
+ALTER TABLE member CHANGE id id VARCHAR(10);
 
+-- (2) age 속성 삭제 
+ALTER TABLE member DROP age;
+
+-- (3) interest 속성 추가
+ALTER TABLE member ADD interest VARCHAR(100); 
+
+DESC member;
+
+DROP TABLE member;
